3d8ddfcc50fca04e18e539f45eb8f41c33f49695
AnitaSalvas <130136433+AnitaSalvas@users.noreply.github.com>
2023-04-12 14:23:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import anderson_ksamp
 
 chat_id = 5072617748 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, p_value = ttest_ind(x, y, equal_var=False)
-    alpha = 0.03
+    p_value = anderson_ksamp([x, y]).pvalue 
+    alpha = 0.05
     return p_value < alpha # Ваш ответ, True или False
